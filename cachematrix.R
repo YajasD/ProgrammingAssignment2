c0c378de36fb85bd2b650b8cbef203dfca5dda4b
Yajas Dwivedi <yajasdwivedi@gmail.com>
2016-07-20 19:47:19 -0700

    First Commit

--- cachematrix.R
+++ cachematrix.R
@@ -34,3 +34,22 @@ cacheSolve <- function(x, ...) {
     m
   
 }
+
+
+# Sample Output:
+
+# > x = rbind(c(1, -2), c(-2, 1))
+# > m = makeCacheMatrix(x)
+# > m$get()
+# [,1] [,2]
+# [1,]    1   -2
+# [2,]   -2    1
+# > cacheSolve(m)
+# [,1]       [,2]
+# [1,] -0.3333333 -0.6666667
+# [2,] -0.6666667 -0.3333333
+# > cacheSolve(m)
+# getting cached data
+# [,1]       [,2]
+# [1,] -0.3333333 -0.6666667
+# [2,] -0.6666667 -0.3333333
